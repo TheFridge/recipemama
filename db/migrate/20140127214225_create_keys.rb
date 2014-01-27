f3cad486ba34e9ccf611d87e3913cd9e456bd80d
Meeka <rrgayhart@gmail.com>
2014-01-27 14:51:00 -0700

    Adds concept of api key to database

--- db/migrate/20140127214225_create_keys.rb
+++ db/migrate/20140127214225_create_keys.rb
@@ -1,7 +1,8 @@
 class CreateKeys < ActiveRecord::Migration
-  def up
-  end
-
-  def down
+  def change
+    create_table :keys do |t|
+      t.string :application_id
+      t.string :application_keys
+    end
   end
 end
