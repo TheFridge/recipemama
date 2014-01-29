95b0ef3d0622ff787fda048e3af0ae17ab47f344
Meeka <rrgayhart@gmail.com>
2014-01-28 23:07:01 -0700

    splits productions

--- config/environments.rb
+++ config/environments.rb
@@ -1,4 +1,4 @@
-configure :production, :development, :test do
+configure :development, :test do
   env = ENV['RACK_ENV'] || "development"
 
   db_settings = YAML.load(ERB.new(File.read(File.join("config", "database.yml"))).result)[env]
@@ -9,6 +9,19 @@ configure :production, :development, :test do
       :username => db_settings["username"],
       :password => db_settings["password"],
       :database => db_settings["database"],
+      :encoding => 'utf8'
+  )
+end
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/recipe_development')
+  
+  ActiveRecord::Base.establish_connection(
+      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
       :encoding => 'utf8'
   )
 end
