5c7550a65f46d1bf4fa469950379ccc101ac9f12
Meeka <rrgayhart@gmail.com>
2014-02-11 20:32:19 -0700

    debugging even more forward

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ActiveRecord::Base
-    has_many :ingredients
+  has_many :ingredients
 
   def create_recipe(args)
     self.name = args[:name]
@@ -16,6 +16,7 @@ class Recipe < ActiveRecord::Base
       i.description = list_item
       i.recipe_id = self.id
       i.save
+      self.ingredients << i
     end
   end
 
