9dba50efeaa4ea3ab1b200dbfbd905768a1ca72c
Meeka <rrgayhart@gmail.com>
2014-02-11 20:10:47 -0700

    debugging even more forward

--- app.rb
+++ app.rb
@@ -27,12 +27,13 @@ post '/by_ingredient' do
   number = rand(0..response['matches'].count - 1)
   #if response['matches'].count > 0
     id = response['matches'][number]['id']
-    get = GetRecipe.new
-    raw_recipe = get.get_response(id)
-    formatted_recipe = search.format_one_recipe(raw_recipe)
-    @recipe = Recipe.new
-    @recipe.create_recipe(formatted_recipe)
-    {recipe: @recipe, ingredients: @recipe.ingredients}.to_json
+    {number => id}.to_json
+    # get = GetRecipe.new
+    # raw_recipe = get.get_response(id)
+    # formatted_recipe = search.format_one_recipe(raw_recipe)
+    # @recipe = Recipe.new
+    # @recipe.create_recipe(formatted_recipe)
+    # {recipe: @recipe, ingredients: @recipe.ingredients}.to_json
   #else
   #   @internal_recipe = Recipe.find_by("ingredient_list like ?", "%#{@ingredient}%")
   #   if @internal_recipe
