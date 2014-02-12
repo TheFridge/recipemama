a6caf534114e96cace6e92f08547980a1ed83e70
Meeka <rrgayhart@gmail.com>
2014-02-11 20:56:59 -0700

    debugging - removed special cases

--- app.rb
+++ app.rb
@@ -31,8 +31,9 @@ post '/by_ingredient' do
     raw_recipe = get.get_response(id)
     formatted_recipe = search.format_one_recipe(raw_recipe)
     formatted_recipe
-    # @recipe = Recipe.new
-    # @recipe.create_recipe(formatted_recipe)
+    @recipe = Recipe.new
+    @recipe.create_recipe(formatted_recipe)
+    Recipe.last
     # @recipe.to_json
     #{recipe: new_recipe, ingredients: new_recipe.ingredients}.to_json
   #else
