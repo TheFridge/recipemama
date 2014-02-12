6917bfd2d2a45972d0f3de14ba8d5437664f49b0
Meeka <rrgayhart@gmail.com>
2014-02-11 21:39:25 -0700

    adds truncation

--- models/recipe.rb
+++ models/recipe.rb
@@ -3,18 +3,18 @@ class Recipe < ActiveRecord::Base
 
   def create_recipe(args)
     self.name = args[:name]
-    self.total_time = args[:total_time]
+    self.total_time = args[:total_time].slice(0..100)
     self.seconds = args[:seconds].to_i
     self.source_url = args[:source_url]
     self.image_url = args[:images][-1]["imageUrlsBySize"].values.last if args[:images]
     self.servings = args[:servings]
     self.yummly_id = args[:yummly_id]
-    self.ingredient_list = args[:basic_ingredients]
+    self.ingredient_list = args[:basic_ingredients].slice(0..100)
     self.save
     if args[:ingredients]
       args[:ingredients].each do |list_item|
         i = Ingredient.new
-        i.description = list_item
+        i.description = list_item.slice(0..100)
         i.recipe_id = self.id
         i.save
       end
